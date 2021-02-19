bc776a2a5c126d22c07e7e4189717ddd12616a91
Jonas Bucher <j.bucher.mn@gmail.com>
2021-02-19 17:38:37 +0100

    Minor

--- make.sh
+++ make.sh
@@ -3,6 +3,6 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd $SCRIPTPATH
 
-meson build || exit 1;
+meson build -Dwlroots:static=true || exit 1;
 ninja -C build || exit 1;
 python setup.py install --user || exit 1;
--- make_wlroots.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cp subprojects/wlroots.syms subprojects/wlroots/
-cd subprojects/wlroots
-meson build && sudo ninja -C build install
-git status
-git checkout -- .
-
