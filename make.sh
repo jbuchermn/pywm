5ad43d63eef6a8ce6af1bb34a80790781cc7cf75
Jonas Bucher <j.bucher.mn@gmail.com>
2021-02-19 18:04:41 +0100

    Simplify static library handling

--- make.sh
+++ make.sh
@@ -3,6 +3,6 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd $SCRIPTPATH
 
-meson build -Dwlroots:static=true || exit 1;
+meson build || exit 1;
 ninja -C build || exit 1;
 python setup.py install --user || exit 1;
