0ca2717eddd1aca0e67cb174d455201ae4c676ce
Jonas Bucher <j.bucher.mn@gmail.com>
2021-12-03 20:19:35 +0100

    Copied a bunch of sway code

--- src/wm/wm_config.c
+++ src/wm/wm_config.c
@@ -1,8 +1,10 @@
-#include "wm/wm_config.h"
+#define _POSIX_C_SOURCE 200112L
+
 #include <stdlib.h>
 #include <string.h>
 #include <wlr/util/log.h>
 
+#include "wm/wm_config.h"
 #include "wm/wm_server.h"
 #include "wm/wm_layout.h"
 #include "wm/wm_seat.h"
@@ -18,8 +20,20 @@ void wm_config_init_default(struct wm_config *config) {
 
     wl_list_init(&config->outputs);
 
-    config->xcursor_theme = NULL;
-    config->xcursor_size = 24;
+    const char *cursor_theme = getenv("XCURSOR_THEME");
+    unsigned cursor_size = 24;
+    const char *env_cursor_size = getenv("XCURSOR_SIZE");
+    if (env_cursor_size && strlen(env_cursor_size) > 0) {
+        errno = 0;
+        char *end;
+        unsigned size = strtoul(env_cursor_size, &end, 10);
+        if (!*end && errno == 0) {
+            cursor_size = size;
+        }
+    }
+
+    config->xcursor_theme = cursor_theme;
+    config->xcursor_size = cursor_size;
 
     config->natural_scroll = true;
     config->tap_to_click = true;
@@ -44,6 +58,14 @@ void wm_config_reconfigure(struct wm_config* config, struct wm_server* server){
     wm_seat_reconfigure(server->wm_seat);
     wm_layout_reconfigure(server->wm_layout);
     wm_server_reconfigure(server);
+
+
+    char cursor_size_fmt[16];
+    snprintf(cursor_size_fmt, sizeof(cursor_size_fmt), "%u", config->xcursor_size);
+    setenv("XCURSOR_SIZE", cursor_size_fmt, 1);
+    if (config->xcursor_theme != NULL) {
+        setenv("XCURSOR_THEME", config->xcursor_theme, 1);
+    }
 }
 
 void wm_config_add_output(struct wm_config *config, const char *name,
