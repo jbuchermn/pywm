3d5fb5466da17461ee5b4cc3908820e02a222640
Jonas Bucher <j.bucher.mn@gmail.com>
2022-01-23 21:42:36 +0100

    Minor

--- flake.nix
+++ flake.nix
@@ -10,7 +10,9 @@
   flake-utils.lib.eachDefaultSystem (
     system:
     let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+      };
     in
     {
       packages.pywm = (
